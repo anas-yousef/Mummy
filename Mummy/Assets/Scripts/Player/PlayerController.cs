6749c8e24734fc41ca4ed0c290c2d732965d1ed8
anas-yousef <anas.malik.yousef@gmail.com>
2021-01-05 05:06:44 +0200

    Added Player

---
+++ Mummy/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private float speed;
+    [SerializeField] private Animator animator;
+    private float horizontalMove = 0f;
+    void Start()
+    {
+        speed = 10f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime;
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+    }
+}
