d39423e5e6da1de7d2eeb73fd70fc8f6c833fa3d
kamalagbaria <62603884+kamalagbaria@users.noreply.github.com>
2021-01-10 19:58:50 +0200

    Pulling Objects Added

---
+++ Mummy/Assets/Scripts/Player/PlayerThrow1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerThrow1 : MonoBehaviour
+{
+    [Header("Settings")]
+    public ToiletPaper1 toiletPaper;
+    [SerializeField] private LineRenderer toiletLine;
+    [SerializeField] private SpringJoint2D jointLine;
+    [SerializeField] private float seconds;
+
+
+    private GameObject target;
+    
+    private void Start()
+    {
+        toiletPaper.gameObject.SetActive(false);
+        toiletLine.enabled = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1") && !toiletPaper.gameObject.activeSelf && !jointLine.enabled) // if the toilet paper is not away 
+        {
+            toiletPaper.Throw();
+            toiletPaper.gameObject.SetActive(true);
+        }
+        if (target != null)
+        {
+            toiletLine.SetPosition(0,transform.position);
+            toiletLine.SetPosition(1, target.transform.position);
+        }
+    }
+
+    public void TargetHit(GameObject hit)
+    {
+        target = hit;
+        target.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
+        toiletLine.enabled = true;
+        jointLine.enabled = true;
+        jointLine.connectedBody = target.GetComponent<Rigidbody2D>();
+        toiletPaper.gameObject.SetActive(false);
+        StartCoroutine(ReleaseAfterSeconds());
+
+    }
+    private IEnumerator ReleaseAfterSeconds()
+    {
+        yield return new WaitForSeconds(seconds);
+        Release();
+    }
+    public void Release()
+    {
+        jointLine.enabled = false;
+        toiletLine.enabled = false;
+        toiletPaper.gameObject.SetActive(true);
+        target.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
+        target = null;
+    }
+}
---
+++ Mummy/Assets/Scripts/ToiletPaper/ToiletPaper1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class ToiletPaper1 : MonoBehaviour
+{
+    [Header("Settings")]
+    public float speed = 3f; // default speed 1 unit / second
+    public float distance = 5f; // default distance 5 units
+    public Transform toiletPaper; // the object you want to throw (assign from the scene)
+    private float _distance; // the distance it moves
+    private bool _back; // is it coming back
+
+    //new
+    [SerializeField] PlayerThrow1 playerScript; //player script
+    private bool _hit; //can get hit
+
+    public void Throw ()
+    {
+        _distance = 0; 
+        _back = false; 
+        enabled = true;
+    }
+ 
+    private void Update ()
+    {
+        float travel = Time.deltaTime * speed;
+        if (!_back)
+        {
+            // TODO need to add the option to throw 135 degrees. 
+            
+            toiletPaper.Translate(new Vector3(1, 1, 0) * travel); // moves object
+            _distance += travel; // update distance
+            _back = _distance >= distance || _hit; // goes back if distance reached
+        }
+        else
+        {
+            toiletPaper.Translate(new Vector3(1, 1, 0) * -travel); // moves object
+            _distance -= travel; // update distance;
+            enabled = _distance > 0; // turning off when done
+            _hit = _distance > 0;
+            
+        }
+    }
+ 
+    private void OnEnable ()
+    {
+       toiletPaper.gameObject.SetActive (true); // activating the object
+
+    }
+    private void OnDisable ()
+    {
+        toiletPaper.gameObject.SetActive (false);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Box" && !_hit)
+        {
+            collision.gameObject.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
+            _hit = true;
+            playerScript.TargetHit(collision.gameObject);
+        }
+    }
+   
+}
