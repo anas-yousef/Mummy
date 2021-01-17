f298f47c03a97d7ac08f5eb1776328ff3f05a8b9
anas-yousef <anas.malik.yousef@gmail.com>
2021-01-17 04:20:18 +0200

    Fixed level

---
+++ Mummy/Assets/Scripts/DraggableCheckPlayer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DraggableCheckPlayer : MonoBehaviour
+{
+    [SerializeField] Rigidbody2D parentRB;
+    [SerializeField] float parentMass;
+    // Start is called before the first frame update
+    void Start()
+    {
+        parentRB = GetComponentInParent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    //void Update()
+    //{
+
+    //}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            parentRB.mass = parentMass;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            parentRB.mass = 5f;
+        }
+    }
+}
