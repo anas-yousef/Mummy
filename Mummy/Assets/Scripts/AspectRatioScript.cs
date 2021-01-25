467aa778f6d9de0d300ee3572e17772c98a39c50
kerenkschneider <kerenix@gmail.com>
2021-01-25 19:08:49 +0200

    changed a lot of things in the panels, tried to add cross resulotion support

---
+++ Mummy/Assets/Scripts/AspectRatioScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AspectRatioScript : MonoBehaviour
+{
+
+    public float targetWidth = 1920;
+    public float targetHeight = 1080;
+    public Camera camera; 
+    
+    void Start()
+    {
+        float windowAspect = (float) Screen.width / (float) Screen.height;
+        float targetAspect = targetWidth / targetHeight;
+        float scaleHeigth = windowAspect / targetAspect;
+        
+        if (windowAspect < targetAspect)
+        {
+            camera.orthographicSize = (targetHeight / 200f) / scaleHeigth;
+        }
+        else
+        {
+            camera.orthographicSize = targetHeight / 200f;
+        }
+    }
+}
--- Mummy/Assets/Scripts/GameManager.cs
+++ Mummy/Assets/Scripts/GameManager.cs
@@ -1,53 +1,91 @@
-﻿using UnityEngine;
+﻿using UnityEditor.U2D;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     [Header("Player")]
     public GameObject player;
     public MummyPaper tp;
+    public Transform all;
 
     [Header("Levels")]
     // todo need to update here every new level 
     public GameObject[] levels = new GameObject[2];
     
-    [Header("Sounds")]
-    // todo need to update here every new level 
-    public AudioSource[] sounds = new AudioSource[2];
-
     [Header("Panels")] 
     public GameObject startPanel;
     public GameObject settingsPanel;
     public GameObject winPanel;
+    public GameObject mainSettingsPanel;
+    public GameObject howToPanel;
+
+    public GameObject mainMenuFirstButton, mainSettingsFirstButton, settingFirstButton, winFirstButton; 
 
 
     private int _curLevel;
     private GameObject _curLevelMap;
     private Transform _curStartLocation;
+    private bool _mainMenu; 
 
     void Start()
     {
         // Manage panels. 
         startPanel.SetActive(true);
         player.SetActive(false);
+        
+        _mainMenu = true; 
     }
 
     private void Update()
     {
-        
-        // Goto options. 
-        if (Input.GetKeyDown(KeyCode.Escape) && !startPanel.activeSelf)
+        // Main menu
+        // Return from main settings to main menu. 
+        if (Input.GetKeyDown(KeyCode.Escape) && mainSettingsPanel.activeSelf && _mainMenu)
         {
-            Time.timeScale = 0.0f;
-            settingsPanel.SetActive(true);
+            // manage panels. 
+            mainSettingsPanel.SetActive(false);
+            BackToMainMenu();
+        }
+        // Return from how to. 
+        else if (Input.GetKeyDown(KeyCode.Escape) && howToPanel.activeSelf)
+        {
+            howToPanel.SetActive(false);
+            BackToMainMenu();
         }
         
-        // Goto options. 
-        if (Input.GetKeyDown(KeyCode.M))
+        // While in game. 
+        // Return from main settings to setting. 
+        else if (Input.GetKeyDown(KeyCode.Escape) && mainSettingsPanel.activeSelf && !_mainMenu)
         {
-            sounds[0].mute = !sounds[0].mute;
-            sounds[1].mute = !sounds[1].mute;
+            // manage panels. 
+            settingsPanel.SetActive(true);
+            mainSettingsPanel.SetActive(false);
+            
+            // chose button marked on screen. 
+            EventSystem.current.SetSelectedGameObject(null);
+            EventSystem.current.SetSelectedGameObject(settingFirstButton);
         }
 
+        // Goto options- while in Game. 
+        else if (Input.GetKeyDown(KeyCode.Escape) && !startPanel.activeSelf && !mainSettingsPanel.activeSelf &&
+                 !howToPanel.activeSelf && !settingsPanel.activeSelf)
+        {
+            settingsPanel.SetActive(true);
+            Time.timeScale = 0.0f;
+            
+            // chose button marked on screen. 
+            EventSystem.current.SetSelectedGameObject(null);
+            EventSystem.current.SetSelectedGameObject(settingFirstButton);
+        }
+        
+        // Return from options- while in Game. 
+        else if (Input.GetKeyDown(KeyCode.Escape) && settingsPanel.activeSelf && !mainSettingsPanel.activeSelf)
+        {
+            BackToLevel();
+        }
+        
     }
 
     /**
@@ -62,12 +100,15 @@ public class GameManager : MonoBehaviour
         _curLevel = 0;
         _curLevelMap = Instantiate(levels[_curLevel]);
         Transform trans = _curLevelMap.transform;
+        trans.parent = all; 
         
         // Locate the player
         _curStartLocation = trans.Find("Player Start Location");
         player.transform.localPosition = new Vector3(_curStartLocation.position.x, _curStartLocation.position.y, 0);        
         
         player.SetActive(true);
+
+        _mainMenu = false; 
     }
 
     /**
@@ -87,6 +128,7 @@ public class GameManager : MonoBehaviour
 
             // relocate the player
             Transform trans = _curLevelMap.transform;
+            trans.parent = all;
             _curStartLocation = trans.Find("Player Start Location");
             
             player.transform.localPosition = new Vector3(_curStartLocation.position.x, _curStartLocation.position.y, 0);
@@ -95,6 +137,8 @@ public class GameManager : MonoBehaviour
         {
             Debug.Log("need to win");
             winPanel.SetActive(true);
+            EventSystem.current.SetSelectedGameObject(null);
+            EventSystem.current.SetSelectedGameObject(winFirstButton);
         }
     }
 
@@ -137,11 +181,18 @@ public class GameManager : MonoBehaviour
 
         // Manage panels.
         settingsPanel.SetActive(false);
+        winPanel.SetActive(false);
         startPanel.SetActive(true);
 
         // Back to start settings. 
         player.SetActive(false);
         _curLevel = 0;
+        
+        // chose button marked on screen. 
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(mainMenuFirstButton);
+
+        _mainMenu = true; 
     }
     
     /**
@@ -155,14 +206,31 @@ public class GameManager : MonoBehaviour
     
     }
     
-    /**
-     * Go back to the level.
-     */
     public void BackToLevel()
     {
         settingsPanel.SetActive(false);
         Time.timeScale = 1.0f;
    
     }
+    public void PressHowTo()
+    {
+        howToPanel.SetActive(true);
+        startPanel.SetActive(false);
 
+    }
+    
+    public void PressMainSettings()
+    {
+        settingsPanel.SetActive(false);
+        startPanel.SetActive(false);
+
+        mainSettingsPanel.SetActive(true);
+        
+        // chose button marked on screen. 
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(mainSettingsFirstButton);
+        
+    }
+    
+    
 }
