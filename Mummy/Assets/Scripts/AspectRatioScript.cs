e3d3923830277d5186bcd74db8053e86a2031b69
kerenkschneider <kerenix@gmail.com>
2021-02-02 11:17:41 +0200

    some level changes, fixed cross resulotion

--- Mummy/Assets/Scripts/AspectRatioScript.cs
+++ Mummy/Assets/Scripts/AspectRatioScript.cs
@@ -8,10 +8,13 @@ public class AspectRatioScript : MonoBehaviour
     public float targetWidth = 1920;
     public float targetHeight = 1080;
     public Camera camera;
-    public Transform bg; 
+    public Transform bg;
+    public Transform main;
+    public Transform[] pannels = new Transform[5];
     
     void Start()
     {
+        float screenH = Screen.height;
         float windowAspect = (float) Screen.width / (float) Screen.height;
         float targetAspect = targetWidth / targetHeight;
         float scaleHeigth = windowAspect / targetAspect;
@@ -19,12 +22,20 @@ public class AspectRatioScript : MonoBehaviour
         if (windowAspect < targetAspect)
         {
             camera.orthographicSize = (targetHeight / 200f) / scaleHeigth;
+
         }
         else
         {
             camera.orthographicSize = targetHeight / 200f;
+            pannels[0].localScale = new Vector3((float) targetHeight / screenH, targetHeight / screenH, 1) ;
+            pannels[1].localScale = new Vector3((float) targetHeight / screenH, targetHeight / screenH, 1) ;
+            pannels[2].localScale = new Vector3((float) targetHeight / screenH, targetHeight / screenH, 1) ;
+            pannels[3].localScale = new Vector3((float) targetHeight / screenH, targetHeight / screenH, 1) ;
+            pannels[4].localScale = new Vector3((float) targetHeight / screenH, targetHeight / screenH, 1) ;
+
         }
         
         bg.localScale = new Vector3(1, 1, 1);
+
     }
 }
